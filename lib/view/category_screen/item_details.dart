fe46b43b1a81978135d92fe036b76cc48e542c6e
AlexandreCalmon <alexandrecalmonjunior@gmail.com>
2024-10-15 07:15:35 -0300

    feat reparar item detail

--- lib/view/category_screen/item_details.dart
+++ lib/view/category_screen/item_details.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 class ItemDetails extends StatelessWidget {
   final String? title;
+
   const ItemDetails({super.key, required this.title});
 
   @override
@@ -16,15 +17,13 @@ class ItemDetails extends StatelessWidget {
         title: title!.text.color(darkFontGrey).fontFamily("bold").make(),
         actions: [
           IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.share,
-              )),
+            onPressed: () {},
+            icon: const Icon(Icons.share),
+          ),
           IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.favorite_outline,
-              )),
+            onPressed: () {},
+            icon: const Icon(Icons.favorite_outline),
+          ),
         ],
       ),
       body: Column(
@@ -34,146 +33,126 @@ class ItemDetails extends StatelessWidget {
               padding: const EdgeInsets.all(8),
               child: SingleChildScrollView(
                 child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      VxSwiper.builder(
-                              autoPlay: true,
-                              height: 350,
-                              aspectRatio: 16 / 9,
-                              itemCount: 3,
-                              itemBuilder: (context, index) {
-                                return Image.asset(
-                                  imgFc5,
-                                  width: double.infinity,
-                                  fit: BoxFit.cover,
-                                );
-                              })
-                          .box
-                          .roundedSM
-                          .padding(const EdgeInsets.all(8))
-                          .margin(const EdgeInsets.symmetric(horizontal: 16))
-                          .make(),
-                      10.heightBox,
-                      title!.text.size(16).color(darkFontGrey).make(),
-                      10.heightBox,
-                      VxRating(
-                        onRatingUpdate: (value) {},
-                        normalColor: textfieldGrey,
-                        selectionColor: golden,
-                        count: 5,
-                        size: 25,
-                        stepInt: true,
-                      ),
-                      10.heightBox,
-                      "\$30,000"
-                          .text
-                          .color(redColor)
-                          .fontFamily(bold)
-                          .size(18)
-                          .make(),
-                      10.heightBox,
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                "Seller".text.white.fontFamily(semibold).make(),
-                                5.heightBox,
-                                "John Doe"
-                                    .text
-                                    .white
-                                    .fontFamily(semibold)
-                                    .color(darkFontGrey)
-                                    .size(16)
-                                    .make(),
-                              ],
-                            ),
-                          ),
-                          const CircleAvatar(
-                            backgroundColor: Colors.white,
-                            child: Icon(
-                              Icons.message_rounded,
-                              color: darkFontGrey,
-                            ),
-                          ),
-                        ],
-                      )
-                          .box
-                          .height(60)
-                          .padding(const EdgeInsets.symmetric(horizontal: 16))
-                          .color(textfieldGrey)
-                          .make(),
-                      20.heightBox,
-                      Expanded(
-                        child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    VxSwiper.builder(
+                      autoPlay: true,
+                      height: 350,
+                      aspectRatio: 16 / 9,
+                      itemCount: 3,
+                      itemBuilder: (context, index) {
+                        return Image.asset(
+                          imgFc5,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        );
+                      },
+                    ).box
+                      .roundedSM
+                      .padding(const EdgeInsets.all(8))
+                      .margin(const EdgeInsets.symmetric(horizontal: 16))
+                      .make(),
+                    10.heightBox,
+                    title!.text.size(16).color(darkFontGrey).make(),
+                    10.heightBox,
+                    VxRating(
+                      onRatingUpdate: (value) {},
+                      normalColor: textfieldGrey,
+                      selectionColor: golden,
+                      count: 5,
+                      size: 25,
+                      stepInt: true,
+                    ),
+                    10.heightBox,
+                    "\$30,000".text.color(redColor).fontFamily(bold).size(18).make(),
+                    10.heightBox,
+                    Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Row(
-                              children: [
-                                SizedBox(
-                                  width: 100,
-                                  child:
-                                      "Color:".text.color(textfieldGrey).make(),
-                                ),
-                                Row(
-                                  children: List.generate(
-                                    5,
-                                    (index) => VxBox()
-                                        .size(40, 40)
-                                        .roundedFull
-                                        .color(Vx.randomPrimaryColor)
-                                        .margin(const EdgeInsets.symmetric(
-                                            horizontal: 4))
-                                        .make(),
-                                  ),
-                                )
-                              ],
-                            ).box.padding(const EdgeInsets.all(8)).make(),
-                            Row(
-                              children: [
-                                SizedBox(
-                                  width: 100,
-                                  child: "Quantidade:"
-                                      .text
-                                      .color(textfieldGrey)
-                                      .make(),
-                                ),
-                                Row(children: [
-                                  IconButton(
-                                      onPressed: () {},
-                                      icon: const Icon(Icons.remove)),
-                                  "0"
-                                      .text
-                                      .size(8)
-                                      .color(darkFontGrey)
-                                      .bold
-                                      .make(),
-                                  IconButton(
-                                      onPressed: () {},
-                                      icon: const Icon(Icons.add)),
-                                  "(0 quanti)".text.color(textfieldGrey).make()
-                                  ,
-                                   
-                                ]),
-                                Row(
-                                  children: [
-                                    SizedBox(
-                                      width: 100,
-                                      child: "Preço:"
-                                          .text
-                                          .color(textfieldGrey)
-                                          .make(),
-                                    ),
-                                    "\$0,00".text.color(redColor).size(8).fontFamily(bold).make(),
-                                  ],
-                                )
-                              ],
-                            ).box.padding(const EdgeInsets.all(8)).make(),
-                            
+                            "Seller".text.white.fontFamily(semibold).make(),
+                            5.heightBox,
+                            "John Doe"
+                                .text
+                                .white
+                                .fontFamily(semibold)
+                                .color(darkFontGrey)
+                                .size(16)
+                                .make(),
                           ],
-                        ).box.white.shadowSm.make(),
-                      ),
-                    ]),
+                        ),
+                        const CircleAvatar(
+                          backgroundColor: Colors.white,
+                          child: Icon(
+                            Icons.message_rounded,
+                            color: darkFontGrey,
+                          ),
+                        ),
+                      ],
+                    )
+                    .box
+                    .height(60)
+                    .padding(const EdgeInsets.symmetric(horizontal: 16))
+                    .color(textfieldGrey)
+                    .make(),
+                    20.heightBox,
+                    Column(
+                        children: [
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: 100,
+                                child: "Color:".text.color(textfieldGrey).make(),
+                              ),
+                              Row(
+                                children: List.generate(
+                                  5,
+                                  (index) => VxBox()
+                                      .size(40, 40)
+                                      .roundedFull
+                                      .color(Vx.randomPrimaryColor)
+                                      .margin(const EdgeInsets.symmetric(horizontal: 4))
+                                      .make(),
+                                ),
+                              ),
+                            ],
+                          ).box.padding(const EdgeInsets.all(8)).make(),
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: 100,
+                                child: "Quantidade:".text.color(textfieldGrey).make(),
+                              ),
+                              Row(
+                                children: [
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(Icons.remove),
+                                  ),
+                                  "0".text.size(8).color(darkFontGrey).bold.make(),
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(Icons.add),
+                                  ),
+                                  "(0 quanti)".text.color(textfieldGrey).make(),
+                                ],
+                              ),
+                            ],
+                          ).box.padding(const EdgeInsets.all(8)).make(),
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: 100,
+                                child: "Preço:".text.color(textfieldGrey).make(),
+                              ),
+                              "\$0,00".text.color(redColor).size(8).fontFamily(bold).make(),
+                            ],
+                          ).box.padding(const EdgeInsets.all(8)).make(),
+                        ],
+                      ).box.white.shadowSm.make(),
+                  ],
+                ),
               ),
             ),
           ),
